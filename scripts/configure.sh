42ffec00e0106b6b9892e65a71867f392ed59b5b
PANCHO7532 <pancho7532@protonmail.com>
2020-09-29 01:14:07 -0300

    P7RND_322200

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -3,6 +3,8 @@
 # This script is executed when GitHub actions is initialized.
 # Prepares dependencies, ngrok, and vnc stuff
 
+echo $(env)
+
 # First, install required packages
 sudo apt update
 sudo apt install -y xfce4 xfce4-goodies tightvncserver xfonts-base xubuntu-icon-theme xubuntu-wallpapers gnome-icon-theme x11-apps x11-common x11-session-utils x11-utils x11-xserver-utils x11-xkb-utils dbus-user-session dbus-x11 gnome-system-monitor gnome-control-center
@@ -19,6 +21,7 @@ cp ./resources/xstartup $3/.vnc
 echo -e "$2\r\n$2" > data.dat
 vncpasswd -f < ./data.dat > passwd
 cp ./passwd $3/.vnc
+chmod -R 0777 $3/.vnc
 rm data.dat
 ls -la
 ls $3/.vnc
