f9489f8a15851b7d56a6e00b12d83c1bf8924f72
PANCHO7532 <pancho7532@protonmail.com>
2020-10-08 18:42:20 -0300

    P7RND_347700

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Reverse Linux VNC
+# Reverse Linux VNC for GitHub Actions
 Enable VNC access on GitHub Actions VMs for general use.
+
+## Usage
+Fork this repository and trigger a build by editing the README.md for example or commiting some file
--- scripts/configure.sh
+++ scripts/configure.sh
@@ -2,11 +2,6 @@
 # Reverse Linux VNC by PANCHO7532
 # This script is executed when GitHub actions is initialized.
 # Prepares dependencies, ngrok, and vnc stuff
-# $1 - NGROK AUTH TOKEN
-# $2 - VNC PASSWORD
-# $3 - HOME DIRECTORY
-
-echo $(env)
 
 # First, install required packages
 sudo apt update
@@ -18,31 +13,12 @@ unzip ngrok-stable-linux-amd64.zip
 chmod +x ngrok
 
 # Third, generate and copy passwd file and xstartup script
-# Alright, script is executing on: (example, ignore if your repo name isn't the same)
-# /home/runner/work/reverse-linux-vnc/reverse-linux-vnc/scripts/configure.sh
-# but, in the yaml i specified the execution so it can stay on the main directory
-# /home/runner/work/reverse-linux-vnc/reverse-linux-vnc
-# Home is /home/runner
 
-mkdir $3/.vnc
-cp ./resources/xstartup $3/.vnc/xstartup
-echo $2 | vncpasswd -f > $3/.vnc/passwd
-sudo chown -R $USER:$USER $3/.vnc
-#echo -e "$2" > data.dat
-#vncpasswd -f < data.dat > output.dat
-#cp ./output.dat $3/.vnc/passwd
-#rm data.dat
-cat $3/.vnc/passwd
-
-# Alright, pre generated passwd file until i figure what the actual goddamn fuck is wrong with the passwd generation on this shit
-# When login into the machine, especify: "runner" as username (if it's required) and "password" as the VNC password, and you should be good to go.
-
-# wget http://boompancho.p7com.net/vnc-resources.zip
-# unzip vnc-resources.zip
-# mkdir $3/.vnc
-# cp passwd $3/.vnc
-# cp xstartup $3/.vnc
+mkdir $HOME/.vnc
+cp ./resources/xstartup $HOME/.vnc/xstartup
+echo $VNC_PASSWORD | vncpasswd -f > $HOME/.vnc/passwd
+chmod 0600 $HOME/.vnc/passwd
 
 # Fourth and last, set up auth token from argument
-./ngrok authtoken $1
+./ngrok authtoken $NGROK_AUTH_TOKEN
 exit
