de4134854616f4d757c2089fd210490f24e26ffc
PANCHO7532 <pancho7532@protonmail.com>
2020-10-08 23:40:42 -0300

    P7RND_913200

---
+++ scripts/configure-legacy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Reverse Linux VNC for GitHub Actions by PANCHO7532
+# This script is executed when GitHub actions is initialized.
+# Prepares dependencies, ngrok, and vnc stuff
+
+# First, install required packages
+sudo apt update
+sudo apt install -y xfce4 xfce4-goodies tightvncserver xfonts-base xubuntu-icon-theme xubuntu-wallpapers gnome-icon-theme x11-apps x11-common x11-session-utils x11-utils x11-xserver-utils x11-xkb-utils dbus-user-session dbus-x11 gnome-system-monitor gnome-control-center
+
+# Second, download ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+chmod +x ngrok
+
+# Third, generate and copy passwd file and xstartup script
+mkdir $HOME/.vnc
+cp ./resources/xstartup $HOME/.vnc/xstartup
+echo $VNC_PASSWORD | vncpasswd -f > $HOME/.vnc/passwd
+chmod 0600 $HOME/.vnc/passwd
+
+# Fourth and last, set up auth token from argument
+./ngrok authtoken $NGROK_AUTH_TOKEN
+exit
--- scripts/configure.sh
+++ scripts/configure.sh
@@ -5,19 +5,23 @@
 
 # First, install required packages
 sudo apt update
-sudo apt install -y xfce4 xfce4-goodies tightvncserver xfonts-base xubuntu-icon-theme xubuntu-wallpapers gnome-icon-theme x11-apps x11-common x11-session-utils x11-utils x11-xserver-utils x11-xkb-utils dbus-user-session dbus-x11 gnome-system-monitor gnome-control-center
+sudo apt install -y xfce4 xfce4-goodies xfonts-base xubuntu-icon-theme xubuntu-wallpapers gnome-icon-theme x11-apps x11-common x11-session-utils x11-utils x11-xserver-utils x11-xkb-utils dbus-user-session dbus-x11 gnome-system-monitor gnome-control-center libpam0g libxt6 libxext6
 
-# Second, download ngrok
+# Second, install TurboVNC
+wget https://ufpr.dl.sourceforge.net/project/turbovnc/2.2.5/turbovnc_2.2.5_amd64.deb
+sudo dpkg -i turbovnc_2.2.5_amd64.deb
+
+# Third, download ngrok
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 chmod +x ngrok
 
-# Third, generate and copy passwd file and xstartup script
+# Fourth, generate and copy passwd file and xstartup script
 mkdir $HOME/.vnc
 cp ./resources/xstartup $HOME/.vnc/xstartup
 echo $VNC_PASSWORD | vncpasswd -f > $HOME/.vnc/passwd
 chmod 0600 $HOME/.vnc/passwd
 
-# Fourth and last, set up auth token from argument
+# Fifth and last, set up auth token from argument
 ./ngrok authtoken $NGROK_AUTH_TOKEN
 exit
