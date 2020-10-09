876ad6e910e94e2f0581bb97d184865d2b657c13
PANCHO7532 <pancho7532@protonmail.com>
2020-10-08 23:41:51 -0300

    P7RND_287850

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -3,7 +3,7 @@
 # This script is executed when GitHub actions is initialized.
 # Prepares dependencies, ngrok, and vnc stuff
 
-# First, install required packages
+# First, install required packages...
 sudo apt update
 sudo apt install -y xfce4 xfce4-goodies xfonts-base xubuntu-icon-theme xubuntu-wallpapers gnome-icon-theme x11-apps x11-common x11-session-utils x11-utils x11-xserver-utils x11-xkb-utils dbus-user-session dbus-x11 gnome-system-monitor gnome-control-center libpam0g libxt6 libxext6
 
